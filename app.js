81ac1369c1c96f66148289783442e44719d6c2d8
Piotr K <piotr.keska67@gmail.com>
2023-03-26 23:25:29 +0200

    add result view

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
+const menuView = document.querySelector('.menu');
+const rateView = document.querySelector('.result');
 const rankItems = [...document.querySelectorAll('.circle')];
 const submitBtn = document.querySelector('button');
+const rankText = document.querySelector('.rank-text');
+const returnBtn = document.querySelector('.return-button')
 
 
+
+let selectedRank;
+
+const toggler = () => {
+    menuView.classList.toggle('hidden');
+    rateView.classList.toggle('hidden');
+}
 const clearSelected = () => {
     rankItems.forEach(item => item.classList.remove('selected'));
 }
 
 
 rankItems.forEach(item => {
-    item.addEventListener('click', () => {
+    item.addEventListener('click', (event) => {
         clearSelected();
-        item.classList.add('selected')
+        item.classList.add('selected');
+        selectedRank = `Your rate is ${event.target.id} out of 5`;
         
     })
 })
 
+submitBtn.addEventListener('click', () => {
+    if(selectedRank === ''){
+        alert('You need choose rank before submit');
+    } else {
+        clearSelected();
+        rankText.textContent = selectedRank;
+        toggler();
+    }
+    selectedRank = '';
+})
 
-
+returnBtn.addEventListener('click', () => {
+    toggler();
+}
+)
